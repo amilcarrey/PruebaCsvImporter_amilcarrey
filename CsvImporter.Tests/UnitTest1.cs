05502dbc7bce76f82ce3ca55b8c51c5b6ed9552a
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-12-13 17:55:24 -0300

    Adding test example

--- CsvImporter.Tests/UnitTest1.cs
+++ CsvImporter.Tests/UnitTest1.cs
@@ -5,10 +5,21 @@ namespace CsvImporter.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
+
+
 }
+
+
