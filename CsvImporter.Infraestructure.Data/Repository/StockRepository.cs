039e033cb5099ecab5a64a01f97178cf6ac58383
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-12-15 12:28:57 -0300

    Adding EF core extensions fo zz module for bulkInsert

--- CsvImporter.Infraestructure.Data/Repository/StockRepository.cs
+++ CsvImporter.Infraestructure.Data/Repository/StockRepository.cs
@@ -1,8 +1,10 @@
 ﻿using CsvImporter.Core;
 using CsvImporter.Core.Entities;
 using CsvImporter.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace CsvImporter.Infraestructure.Data
@@ -13,10 +15,12 @@ namespace CsvImporter.Infraestructure.Data
         public StockRepository(AcmeContext context)
         {
             _context = context;
+            _context.ChangeTracker.AutoDetectChangesEnabled = false;            
         }
         public void Clear()
         {
-            throw new NotImplementedException();
+            _context.Database.ExecuteSqlRaw("TRUNCATE TABLE STOCK");
+            _context.SaveChanges();
         }
 
         public void Create(StockModel stock)
@@ -24,5 +28,30 @@ namespace CsvImporter.Infraestructure.Data
             _context.Stock.Add(stock);
             _context.SaveChanges();
         }
+
+        public async System.Threading.Tasks.Task CreateAsync(List<StockModel> listStock)
+        {
+            await _context.Stock.BulkInsertAsync(listStock);
+            DetachAll();
+        }
+
+        public async System.Threading.Tasks.Task SaveChangeAsync()
+        {
+            await _context.SaveChangesAsync();            
+        }
+
+        public void DetachAll()
+        {
+            var changedEntriesCopy = _context.ChangeTracker.Entries()
+            .Where(e => e.State == EntityState.Added ||
+                        e.State == EntityState.Modified ||
+                        e.State == EntityState.Deleted ||
+                        e.State == EntityState.Unchanged)
+            .ToList();
+
+            foreach (var entry in changedEntriesCopy)
+                entry.State = EntityState.Detached;
+        }
+        
     }
 }
---
+++ CsvImporter.Infraestructure.Data/Services/BulkUploadToSql.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvImporter.Infraestructure.Data.Services
+{
+    public class BulkUploadToSql
+    {
+
+    }
+}
