8a7c35e9556162795b91558162531f3d6e6abe14
Amilcar Rey <amilcar.rey@aces.com.ar>
2020-12-16 07:25:41 -0300

    Adding BlobStorage

--- CsvImporter.Infraestructure.Data/Services/CsvFromAzureBlobStorage.cs
+++ CsvImporter.Infraestructure.Data/Services/CsvFromAzureBlobStorage.cs
@@ -1,4 +1,7 @@
-﻿using CsvImporter.Core.Interfaces;
+﻿using Azure.Storage.Blobs;
+using CsvImporter.Core.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -8,9 +11,30 @@ namespace CsvImporter.Infraestructure.Data.Services
 {
     class CsvFromAzureBlobStorage
     {
-        public Stream GetCSVStream()
+        private readonly IConfiguration _configuration;
+        private ILogger<BulkUploadToSql> _logger;
+
+        public CsvFromAzureBlobStorage(IConfiguration configuration, ILogger<BulkUploadToSql> logger)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+            _logger = logger;
+        }
+        public Stream GetCSVStream(string key, string container, string fileName)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(key);
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(container);
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            if (blobClient.Exists())
+            {
+                _logger.LogInformation("Cliente creado, obteniendo stream...");
+                return blobClient.OpenRead();
+            }
+            else
+            {
+                _logger.LogError("Cliente de Blob storage no encontrado.");
+                throw new InvalidOperationException("El cliente no existe");
+            }
         }
     }
 }
